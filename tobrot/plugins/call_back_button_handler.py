601630c3b7cc04973adfab426adceed6caa4912c
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2021-07-08 11:32:55 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -33,7 +33,7 @@ async def button(bot, update: CallbackQuery):
         else:
             await bot.answer_callback_query(
                 callback_query_id=update.id,
-                text="who are you? 🤪🤔🤔🤔",
+                text="pakkaki poyi aduko",
                 show_alert=True,
                 cache_time=0,
             )
@@ -83,7 +83,7 @@ async def button(bot, update: CallbackQuery):
         else:
             await bot.answer_callback_query(
                 callback_query_id=update.id,
-                text="who are you? 🤪🤔🤔🤔",
+                text="pakkaki poyi aduko",
                 show_alert=True,
                 cache_time=0,
             )
